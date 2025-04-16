2b3c8764e7ff76e8d19a41a8c38a06c606a67184
karanzhere <35666568+karanzhere@users.noreply.github.com>
2025-04-16 18:27:14 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,10 @@
+#Task 1: Check if a Number is Even or Odd
+
+nums = int(input("Enter a number: "))
+
+num_check = nums%2
+
+if num_check == 0:
+    print("It's an Even Number")
+else:
+    print("It's an Odd number")
---
+++ Task2.py
@@ -0,0 +1,9 @@
+# Initialize the sum variable
+sum = 0
+
+# Iterate over numbers from 1 to 50
+for series in range(1, 51):
+    sum += series
+
+# Display the final sum
+print(f"The sum of all integers from 1 to 50 is: {sum}")
